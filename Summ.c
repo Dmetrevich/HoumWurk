3d942092f970603e8cae627a53a2701a1cdaaa16
Dmitriy Mariich <dmeter1521@gmail.com>
2024-06-16 10:38:16 +0500

    Ok_scanf

--- Summ.c
+++ Summ.c
@@ -1,20 +1,14 @@
+
 #include <stdio.h>
 
 int main()
 {
-    int a,b,c;
-    a = 4;
-    b = 5;
-    c = 7;
-     printf("%d+%d+%d=%d\n",a,b,c,a+b+c);
-     //=============Test#2================
-    printf("\n");
-     int d = 10, e = 20, f = 30;
-    
-    printf("%d+%d+%d=%d\n",d,e,f,d+e+f);
-    printf("\n");  
-    
-     
+	int a,b,c,d;
+	scanf("%d%d%d", &a, &b, &c);
+	d=a+b+c;
+	printf("%d+%d+%d=%d", a,b,c,d);
 
-    return 0;
+
+
+	return 0;
 }
